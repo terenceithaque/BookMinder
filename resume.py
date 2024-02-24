724a28e2c7b0de2deeed9303d47186d7a4b361c1
Terence Le Thierry <terence.lethierry@protonmail.com>
2024-02-24 15:47:32 +0100

    Ajouté la fonction extraire_resume pour trouver le résumé d'un livre à partir d'un titre et d'une langue donnée

--- resume.py
+++ resume.py
@@ -2,6 +2,7 @@
 import newspaper # Importation du module newspaper
 
 
+
 url = "https://www.wikiwand.com" # URL à partir de laquelle on extraiera le résumé du livre
 
 
@@ -18,5 +19,18 @@ def titre_en_url(titre):
             titre_converti += "_" # L'URL du à partir de laquelle on extrait le résumé contient un underscore après chaque mot du titre, donc on ajoute un underscore au titre converti après chaque mot sauf le dernier
 
     return titre_converti
+
+
+def extraire_resume(titre, langue):
+    "Extraire le résumé d'un livre à partir de son titre"
+    url_resume = url + "/" + langue + "/" + titre_en_url(titre) # On forme l'url complète pour l'extraction du résumé à partir de la variable url + un slash + la langue du titre +  le titre converti conformément
+    
+    article = newspaper.Article(url_resume) # On crée un nouvel objet Article à partir de l'URL
+    article.download() # On télécharge le contenu HTMl de l'article
+    article.parse()
+
+    print(article.text) # On affiche le résumé du livre
+
+    return article.text
 
 
