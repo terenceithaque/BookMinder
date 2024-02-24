4bf45d6908473f7f08d06981e5e8685246e26793
Terence Le Thierry <terence.lethierry@protonmail.com>
2024-02-24 15:51:02 +0100

    Ajout du fichier ajouter_lecture.py

---
+++ ajouter_lecture.py
@@ -0,0 +1,4 @@
+# Script pour ajouter une lecture
+from tkinter import * # Importation de tkinter pour l'interface graphique
+from tkinter import filedialog # Importation du module filedialog de tkinter pour dialoguer avec les fichiers
+from resume import * 
