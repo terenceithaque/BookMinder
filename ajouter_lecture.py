8ded41fab167ef3533f14fdbc0a2af5206298bd0
Terence Le Thierry <terence.lethierry@protonmail.com>
2024-02-24 16:17:34 +0100

    Ajouté plusieures entrées pour permettre à l'utilisateur de saisir le titre, l'année et l'auteur(e) d'un livre dans FenetreAjouter

--- ajouter_lecture.py
+++ ajouter_lecture.py
@@ -11,3 +11,24 @@ class FenetreAjouter(Toplevel):
         super().__init__() # On hérite de la classe Toplevel de tkinter
 
         self.fenetre_maitre = fenetre_maitre # Fenêtre maître
+
+        self.label_titre = Label(self, text="Titre du livre :") # Label pour demander à l'utilisateur de saisir le titre du livre
+
+        self.label_titre.pack(fill="both")
+
+        self.titre_livre = Entry(self) # Entrée pour saisir le titre du livre
+
+        self.titre_livre.pack(fill="both")
+
+        self.label_annee = Label(self, text="Année de publication :") # Label pour demander à l'utilisateur de saisir l'année de publication du livre (optionnel)
+        self.label_annee.pack(fill="both")
+
+        self.annee_livre = Entry(self) # Entrée pour saisir l'année de publication du livre
+        self.annee_livre.pack(fill="both")
+
+        self.label_auteur = Label(self, text="Auteur(e) du livre :") # Label pour demander à l'utilisateur de saisir l'auteur(e) du livre (optionnel)
+        self.label_auteur.pack(fill="both")
+
+        self.auteur_livre = Entry(self)  # Entrée pour saisir le nom de l'auteur(e) du livre
+        self.auteur_livre.pack(fill="both")
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Script principal
 from tkinter import * # Importation de tkinter pour l'interface graphique
+from ajouter_lecture import *
 
 class Application(Tk):
     "Classe représentant une instance de l'application. Elle hérite de la classe Tk de tkinter"
@@ -15,7 +16,7 @@ class Application(Tk):
 
         self.menu_lecture = Menu(self.barre_menus, tearoff=0) # On ajoute un menu lecture à la barre de menus. Il sert entre autre à ajouter un livre lu.
 
-        self.menu_lecture.add_command(label="Nouvelle lecture...", command=None) # On ajoute une commande qui permet d'ajouter un livre dans les lectures au menu lecture
+        self.menu_lecture.add_command(label="Nouvelle lecture...", command=lambda:FenetreAjouter(self)) # On ajoute une commande qui permet d'ajouter un livre dans les lectures au menu lecture
 
         self.barre_menus.add_cascade(label="Lecture", menu=self.menu_lecture) # On insère le menu lecture dans la barre de menus
 
