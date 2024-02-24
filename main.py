875b851e6a2324f613d7f2d9bda466d92e785d34
Terence Le Thierry <terence.lethierry@protonmail.com>
2024-02-24 15:24:03 +0100

    Ajouté une barre de menus et un menu lecture à l'application

--- main.py
+++ main.py
@@ -4,5 +4,24 @@ from tkinter import * # Importation de tkinter pour l'interface graphique
 class Application(Tk):
     "Classe représentant une instance de l'application. Elle hérite de la classe Tk de tkinter"
 
-    def __ini__(self):
+    def __init__(self):
+        "Constructeur de l'application"
+
         super().__init__() # On hérite de la classe Tk
+
+        self.title("BookMinder") # Titre de la fenêtre d'application
+
+        self.barre_menus = Menu(self, tearoff=0) # On ajoute une barre de menus à l'application 
+
+        self.menu_lecture = Menu(self.barre_menus, tearoff=0) # On ajoute un menu lecture à la barre de menus. Il sert entre autre à ajouter un livre lu.
+
+        self.menu_lecture.add_command(label="Nouvelle lecture...", command=None) # On ajoute une commande qui permet d'ajouter un livre dans les lectures au menu lecture
+
+        self.barre_menus.add_cascade(label="Lecture", menu=self.menu_lecture) # On insère le menu lecture dans la barre de menus
+
+        self.config(menu = self.barre_menus) # On configure le menu de la fenêtre comme étant la barre de menus qu'on a créée
+
+
+app = Application() # On crée une nouvelle instance d'application
+
+app.mainloop() # On exécute la boucle principale
