994d8feb407936c4c608c1b33e41a9bbc216f415
Terence Le Thierry <terence.lethierry@protonmail.com>
2024-02-24 15:54:32 +0100

    Ajouté la classe FenetreAjouter qui permet d'ajouter une lecture

--- ajouter_lecture.py
+++ ajouter_lecture.py
@@ -2,3 +2,12 @@
 from tkinter import * # Importation de tkinter pour l'interface graphique
 from tkinter import filedialog # Importation du module filedialog de tkinter pour dialoguer avec les fichiers
 from resume import * 
+
+
+class FenetreAjouter(Toplevel):
+    "Classe représentant une fenêtre permettant d'ajouter une lecture"
+    def __init__(self, fenetre_maitre):
+        "Constructeur de FenetreAjouter"
+        super().__init__() # On hérite de la classe Toplevel de tkinter
+
+        self.fenetre_maitre = fenetre_maitre # Fenêtre maître
