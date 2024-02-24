197c53787e8e7ff416ddc52a6bce6e1234474722
Terence Le Thierry <terence.lethierry@protonmail.com>
2024-02-24 15:16:59 +0100

    Ajout de la classe Application dans main.py. Elle hérite de la classe Tk et représente une fenêtre d'application

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 # Script principal
 from tkinter import * # Importation de tkinter pour l'interface graphique
+
+class Application(Tk):
+    "Classe représentant une instance de l'application. Elle hérite de la classe Tk de tkinter"
+
+    def __ini__(self):
+        super().__init__() # On hérite de la classe Tk
