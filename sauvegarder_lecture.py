383b2ebcf279eb6cc6afebb1b5dd2d52b1e2aff2
Terence Le Thierry <terence.lethierry@protonmail.com>
2024-03-02 12:33:43 +0100

    La fonction creer_fichier_chemin n'écrit que le chemin pointant vers un fichier JSON

--- sauvegarder_lecture.py
+++ sauvegarder_lecture.py
@@ -20,7 +20,7 @@ def enregistrer_lecture(titre_livre, annee_livre, auteur_livre, langue_titre, re
     if not os.path.exists("paths"): # Si le dossier paths n'a pas été créé
         creer_dossier_paths() # Créer le dossier paths, qui contient des fichiers texte dont le contenu est un chemin vers un fichier json
 
-    chemin_fichier = filedialog.asksaveasfile(title="Où voulez-vous enregistrer votre lecture ?", filetypes=[("Base de données JSON", "*.json")]) # Demander à l'utilisateur où est-ce qu'il veut enregistrer le fichier JSON
+    chemin_fichier = filedialog.asksaveasfilename(title="Où voulez-vous enregistrer votre lecture ?", filetypes=[("Base de données JSON", "*.json")]) # Demander à l'utilisateur où est-ce qu'il veut enregistrer le fichier JSON
     creer_fichier_chemin(titre_livre, chemin_fichier) # Créer un fichier texte contenant le chemin du fichier JSON nouvellement créé
 
     
